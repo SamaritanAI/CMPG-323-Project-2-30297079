f9178052f41a9ba801069292a06746a928b4aafa
SamaritanAI <dithipekoketso@gmail.com>
2023-08-31 15:54:19 +0200

    added weather controller

---
+++ Controllers/WeatherForecast.cs
@@ -0,0 +1,31 @@
+﻿/*using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMPG_323_Project_2_30297079.Controllers
+{
+    public class WeatherForecast
+    {
+    }
+}*/
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebAppSecureClass.Controllers
+
+{
+
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController>? _logger;
+    }
+
+}
